32a58328d1d4336a76595b499e68e9b76dad020d
DavideRigo <62156893+DavideRigo@users.noreply.github.com>
2025-09-21 20:00:07 +0000

    Deploying to gh-pages from @ DavideRigo/DavideRigo.github.io@07e5ae8c37628b20cd50aef59cbf584cd51522e2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/funding/";
           },
+        },{id: "nav-dissemination",
+          title: "dissemination",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/dissemination/";
+          },
         },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
